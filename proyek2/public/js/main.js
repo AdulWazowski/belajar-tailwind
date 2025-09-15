50eb323484347e450d4c699e2a4751caaed824a5
AdulWazowski <taufikhan17@gmail.com>
2025-09-16 01:38:57 +0700

    udahan dah ngantuk ygy

---
+++ proyek2/public/js/main.js
@@ -0,0 +1,17 @@
+// Fungsi load file HTML eksternal ke dalam elemen target
+async function loadComponent(targetId, file) {
+  try {
+    const res = await fetch(file);
+    if (!res.ok) throw new Error(`Gagal load ${file}`);
+    const html = await res.text();
+    document.getElementById(targetId).innerHTML = html;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+// Load header & footer
+window.addEventListener("DOMContentLoaded", () => {
+  loadComponent("header", "./layout/header.html");
+  loadComponent("footer", "footer.html");
+});
